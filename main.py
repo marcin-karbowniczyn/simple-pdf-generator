0f50bfcf1d8b89501f1b9e749d4311c9b01b6145
Marcin <marcin.karbowniczyn@gmail.com>
2023-11-20 15:22:47 +0100

    Add footer

--- main.py
+++ main.py
@@ -8,19 +8,30 @@ df = pd.read_csv('files/topics.csv')
 
 # Initiate the PDF file
 pdf = FPDF(orientation='P', unit='mm', format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
 
 # Generate pages in the PDF file. This loop will give us access to each row in DF
 for index, row in df.iterrows():
     pdf.add_page()
+
+    # Set the header
     pdf.set_font(family='Times', style='B', size=24)
     pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
+    pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)  # Tutaj height nie jest w mm, ale w points
     # x1 - starting point, y1 - wysokość od x1, x2 - ending point, y2 - wysokość od ending point
     pdf.line(10, 21, 200, 21)
 
-    num_of_pages = int(row['Pages'])
-    if num_of_pages > 1:
-        for n in range(num_of_pages - 1):
-            pdf.add_page()
+    # Set the footer
+    pdf.ln(265)  # Add a breakline which has 265 mm
+    pdf.set_font(family='Times', style='I', size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
+
+    for n in range(row['Pages'] - 1):
+        pdf.add_page()
+        pdf.ln(277)  # Add a breakline which has 277 mm
+        pdf.set_font(family='Times', style='I', size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
 
 pdf.output('output.pdf')
